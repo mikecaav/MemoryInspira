486dc4879cd7ad4390edbcb31c3a96d7df101541
Miguel Caballero <caballero.mc98@gmail.com>
2021-03-31 18:55:40 -0600

    Added redux base structure with empty files

--- src/Components/Memory.js
+++ src/Components/Memory.js
@@ -1,21 +1,28 @@
-import {Component} from "react";
-import {Col, Row} from "react-bootstrap";
-import {Card} from "./Card";
+import {Component} from "react"
+import {Col, Row} from "react-bootstrap"
+import {Card} from "./Card"
 
 export class Memory extends Component {
     render() {
+        // let filenames = [
+        //     "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "31",
+        // ]
         return(
             <Row>
-                <Col xs={6} md={6}>
+                <Col xs={2} md={2}>
                     <Card/>
                 </Col>
-                <Col xs={6} md={6}>
-                    <Card/>
-                </Col>
-                <Col xs={6} md={6}>
-                    <Card/>
-                </Col>
+                {
+                    filenames.map((filename)=>{
+                        filename = filename.split(".jpg")[0].split("/")[1]
+                        return(
+                            <Col xs={2} md={2} filename={filename}>
+                                <Card/>
+                            </Col>
+                        )
+                    })
+                }
             </Row>
-        );
+        )
     }
 }
