a7e5c66146eb664c87d86251c3550125f019dc2b
Miguel Caballero <caballero.mc98@gmail.com>
2021-04-02 20:12:57 -0600

    Implemented uuid for key component

--- src/Components/Memory.js
+++ src/Components/Memory.js
@@ -1,17 +1,18 @@
 import {Col, Row} from "react-bootstrap"
 import {Card} from "./Card"
 import {useSelector} from "react-redux"
+import { v4 as uuidv4 } from 'uuid';
+
 
 export const Memory = () => {
     const filenames = useSelector(state => state.filenameReducer)
-    return(
+    return (
         <Row>
             {
-                filenames.map((filename)=>{
-                    filename = filename.split(".jpg")[0].split("/")[1]
-                    return(
-                        <Col xs={2} md={2} filename={filename}>
-                            <Card/>
+                filenames.map((filename) => {
+                    return (
+                        <Col xs={2} md={2} key={uuidv4()}>
+                            <Card filename={filename}/>
                         </Col>
                     )
                 })
