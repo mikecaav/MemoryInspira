e27c68dc70ba22a742154e959df81e6e6fb80a75
Miguel Caballero <caballero.mc98@gmail.com>
2021-04-05 13:06:19 -0500

    Updated card reducer to support state object instead of object list

--- src/redux/reducers/card.js
+++ src/redux/reducers/card.js
@@ -1,21 +1,29 @@
 import {type as toggleStateType} from "../actions/toggleState"
 import {type as initCardType} from "../actions/initCard"
 
-const defaultState = []
+const defaultState = {}
+const defaultHiddenCardFilename = "back"
 
 const cardReducer = (state=defaultState, action) => {
     switch (action.type){
         case initCardType:
-            state.push(action.payload)
+            action.payload = {
+                ...action.payload,
+                showFilename: defaultHiddenCardFilename
+            }
+            state[action.payload.uuid] = action.payload
             return state
 
         case toggleStateType:
-            state.map((card) => {
-                if (card.uuid === action.payload){
-                    card.isHidden = !card.isHidden
-                }
-            })
+            const uuid = action.payload
+            state[uuid].isHidden = !state[uuid].isHidden
+            if (state[uuid].isHidden){
+                state[uuid].showFilename = defaultHiddenCardFilename
+            }else {
+                state[uuid].showFilename = state[uuid].filename
+            }
             return state
+
         default:
             return state
     }
