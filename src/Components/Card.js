bae05664ed4063bf1669b72593d405c6e40afa84
Miguel Caballero <caballero.mc98@gmail.com>
2021-04-05 13:06:41 -0500

    Updated card object to support card swipes

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,21 @@
 import {Image} from "react-bootstrap"
 import {initCard} from "../redux/actions/initCard"
-import {useDispatch} from "react-redux"
+import {useDispatch, useSelector} from "react-redux"
 import {toggleState} from "../redux/actions/toggleState"
 
 export const Card = ({filename, uuid}) => {
     const dispatch = useDispatch()
     dispatch(initCard({uuid, filename, isHidden:true}))
+    const cardReducer = useSelector(state => state.cardReducer)
 
-    return <Image src={`images/${filename}.jpg`}
-                  rounded
-                  style={{
-                      height: "100", width: "100%",
-                  }}
-                  onClick= {()=>dispatch(toggleState(uuid))}
-    />
+    return (
+
+        <Image src={`./images/${cardReducer[uuid].showFilename}.jpg`}
+               rounded
+               style={{
+                   height: "50", width: "100%",
+               }}
+               onClick= { () => dispatch(toggleState(uuid)) }
+        />
+    )
 }
